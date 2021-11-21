d08c3ea2d863dd665c73b4a2bb72273b3fe798ad
Ardiel <salatamos.ardiel@gmail.com>
2021-11-21 20:12:20 +0800

    tanggalin si tag\

--- admin/update_announcement.php
+++ admin/update_announcement.php
@@ -9,6 +9,36 @@
 	$sql = "SELECT * FROM `announcement_tbl` WHERE announcement_id = '$ann'";
 	$res = mysqli_query($db, $sql);
 	$show_announce = mysqli_fetch_assoc($res);
+
+	if (isset($_POST['update'])) {
+		
+		$up_content = mysqli_real_escape_string($db, $_POST['content']);
+
+		$sql2 = "UPDATE `announcement_tbl` SET `content`='$up_content' WHERE announcement_id = '$ann'";
+		mysqli_query($db, $sql2);
+
+		if($_FILES['image']['name'] != ""){
+
+			$up_file = $_FILES['image']['name'];
+			$tmp_name = $_FILES['image']['tmp_name'];
+			$div = explode('.', $up_file);
+			$ext = strtolower(end($div));
+			$unique_name = md5($div[0].time()).'.'.$ext;
+
+			$destination ="../img/announcement/".$unique_name;
+			$destination1 = "img/announcement/".$unique_name;
+			move_uploaded_file($tmp_name, $destination);
+
+			$sql3 = "UPDATE `announcement_tbl` SET `image`='$destination1' WHERE announcement_id = '$ann'";
+			mysqli_query($db, $sql3);
+
+			echo  '<script> window.location.href="update_announcement.php?id='.$ann.'";</script>';
+
+		}
+		
+		echo  '<script> window.location.href="update_announcement.php?id='.$ann.'";</script>';
+
+	}
 ?>
 <style>
 	.b{
@@ -31,7 +61,9 @@
 		  	<div class="panel-heading" style="height: 55px"></div>
 		  	<div class="panel-body">
 
-		  		<form method="POST">
+		  		<form method="POST" enctype="multipart/form-data">
+
+
 		  			<div class="form-group">
 		  				<label for="content">Content</label>
 		  				<textarea class="form-control" placeholder="content here" name="content" rows="6"><?php echo $show_announce['content']; ?></textarea>
